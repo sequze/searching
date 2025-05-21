e3adc23845d81d7867932c5cbd2a0d43374fd7b7
Амир Абдрафиков <sequze@mail.ru>
2025-05-21 19:06:06 +0300

    created base configuration

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from pydantic import BaseModel
+
+
+# class RunConfig(BaseModel):
+#     host: str = "0.0.0.0"
+#     port: int = 8000
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class Settings(BaseSettings):
+    # run: RunConfig = RunConfig()
+    api_prefix: ApiPrefix = ApiPrefix()
+
+
+settings = Settings()
