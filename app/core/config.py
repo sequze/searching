71f10eb18bb1ea5f64f7130909ccef13e4721bed
Амир Абдрафиков <sequze@mail.ru>
2025-05-28 14:34:09 +0300

    finished connecting db

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import BaseModel, Extra
 from pydantic import PostgresDsn
+from pydantic_settings import SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -14,11 +15,19 @@ class ApiPrefix(BaseModel):
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
-    echo: bool = False,
-    pool_size: int = 50,
-    max_overflow: int = 10,
+    echo: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+        case_sensitive=False,
+        env_nested_delimiter="__",  # разделитель
+        env_prefix="FASTAPI__",
+    )
     run: RunConfig = RunConfig()
     api_prefix: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import uvicorn
 from api import router as api_router
 from core.config import settings
+from core.models import db_helper
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shotdown
+    db_helper.dispose()
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix=settings.api_prefix.prefix)
 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
-
+    
